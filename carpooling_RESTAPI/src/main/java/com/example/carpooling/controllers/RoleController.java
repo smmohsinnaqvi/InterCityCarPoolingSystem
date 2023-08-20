5d9902c25edb96803c9c5bb1c88bfe846f13e5bc
smmohsinnaqvi <heyurmohsin@gmail.com>
2023-08-21 02:58:06 +0530

    Updated Changes on 20th

---
+++ carpooling_RESTAPI/src/main/java/com/example/carpooling/controllers/RoleController.java
@@ -0,0 +1,26 @@
+package com.example.carpooling.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.carpooling.models.Role;
+import com.example.carpooling.services.RoleService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class RoleController 
+{
+
+	@Autowired
+	RoleService rservice;
+	
+	@GetMapping("/getallroles")
+	public List<Role> getAllRoles()
+	{
+		return rservice.getRoles();
+	}
+}
--- carpooling_RESTAPI/src/main/java/com/example/carpooling/models/User.java
+++ carpooling_RESTAPI/src/main/java/com/example/carpooling/models/User.java
@@ -1,106 +1,178 @@
 package com.example.carpooling.models;
 
+import java.sql.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="users")
-public class User {
-	
+@Table(name = "users")
+public class User 
+{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int uid;
-	@Column
-	private String email;
-	@Column
-	private String password;
-	@Column
-	private String fname;
-	@Column
-	private String lname;
+	int id;
+	@Column(length=100)
+	String password;
+	@Column(length=100)
+	String fname;
+	@Column(length=100)
+	String lname;
+	@Column(length=45)
+	String gender;
 	
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	@Column
+	Date dob;
+	
+	@Column(length=45)
+	String aadhar;
+	
+	@Column(length=45)
+	String licence;
+	
+	@Column(length=45)
+	String phone_no;
+	
+	@Column(length=100)
+	String primary_email;
+	
+	@Column(length=100)
+	String secondary_email;
+	
+	@OneToOne
+	@JoinColumn(name="user_id")
+	Login user_id;
+
 	
 	public User() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
-
-	public User(int uid, String email, String password, String fname, String lname) {
-		super();
-		this.uid = uid;
-		this.email = email;
-		this.password = password;
-		this.fname = fname;
-		this.lname = lname;
-	}
+	
 	
 
-
-	public int getUid() {
-		return uid;
+	public User(String fname, String lname, String gender, Date dob, String aadhar, String licence, String phone_no,
+			String primary_email, String secondary_email, Login user_id) {
+		super();
+		this.fname = fname;
+		this.lname = lname;
+		this.gender = gender;
+		this.dob = dob;
+		this.aadhar = aadhar;
+		this.licence = licence;
+		this.phone_no = phone_no;
+		this.primary_email = primary_email;
+		this.secondary_email = secondary_email;
+		this.user_id = user_id;
 	}
 
 
-	public void setUid(int uid) {
-		this.uid = uid;
+
+	public int getId() {
+		return id;
 	}
 
-
-	public String getEmail() {
-		return email;
+	public void setId(int id) {
+		this.id = id;
 	}
-
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
 
 	public String getPassword() {
 		return password;
 	}
 
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 
 	public String getFname() {
 		return fname;
 	}
 
-
 	public void setFname(String fname) {
 		this.fname = fname;
 	}
-
 
 	public String getLname() {
 		return lname;
 	}
 
-
 	public void setLname(String lname) {
 		this.lname = lname;
 	}
 
+	public String getGender() {
+		return gender;
+	}
 
-	@Override
-	public String toString() {
-		return "User [uid=" + uid + ", email=" + email + ", password=" + password + ", fname=" + fname + ", lname="
-				+ lname + "]";
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public String getAadhar() {
+		return aadhar;
+	}
+
+	public void setAadhar(String aadhar) {
+		this.aadhar = aadhar;
+	}
+
+	public String getLicence() {
+		return licence;
+	}
+
+	public void setLicence(String licence) {
+		this.licence = licence;
+	}
+
+	public String getPhone_no() {
+		return phone_no;
+	}
+
+	public void setPhone_no(String phone_no) {
+		this.phone_no = phone_no;
+	}
+
+	public String getPrimary_email() {
+		return primary_email;
+	}
+
+	public void setPrimary_email(String primary_email) {
+		this.primary_email = primary_email;
+	}
+
+	public String getSecondary_email() {
+		return secondary_email;
+	}
+
+	public void setSecondary_email(String secondary_email) {
+		this.secondary_email = secondary_email;
+	}
+
+	public Login getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(Login user_id) {
+		this.user_id = user_id;
 	}
 	
 	
 	
-	
-	
-	
-
 }
