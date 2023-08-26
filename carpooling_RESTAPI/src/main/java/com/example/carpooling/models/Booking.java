4d9cdbfa0528e58301ea75778d00feb8a1c7565a
nikhilmadhekar <madhekarnikhil141@gmail.com>
2023-08-26 21:53:57 +0530

    added

---
+++ carpooling_RESTAPI/src/main/java/com/example/carpooling/models/Booking.java
@@ -0,0 +1,167 @@
+package com.example.carpooling.models;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "booking")
+public class Booking 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+	
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+	@Column
+	LocalDateTime time;
+	
+	@Column
+	int no_of_seats;
+	
+	@Column
+	int total_price;
+	
+	@Column(length = 100)
+	String status;
+	
+	@JsonIgnoreProperties("bookings")
+	
+	@ManyToOne
+	@JoinColumn(name = "ride_id")
+	Ride rides;
+	
+	@ManyToOne
+	@JoinColumn(name = "passenger_id")
+	User users;
+	
+//	@ManyToMany(cascade = {CascadeType.MERGE,CascadeType.PERSIST})
+//	@JoinTable(name = "payments_bookings",
+//				joinColumns = @JoinColumn(name="booking_id") ,
+//				inverseJoinColumns = @JoinColumn(name="payment_id")	
+//			)
+//	Set<Payment> payments;
+
+	public Booking() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
+//	public Booking(LocalDateTime time, int no_of_seats, int total_price, String status, Ride rides,
+//			Set<Payment> payments) {
+//		super();
+//		this.time = time;
+//		this.no_of_seats = no_of_seats;
+//		this.total_price = total_price;
+//		this.status = status;
+//		this.rides = rides;
+//		this.payments = payments;
+//	}
+	
+	public Booking(int id) {
+		super();
+		this.id = id;
+	}
+
+
+	public Booking(int id, LocalDateTime time, int no_of_seats, int total_price, String status, Ride rides,
+			User users) {
+		super();
+		this.id = id;
+		this.time = time;
+		this.no_of_seats = no_of_seats;
+		this.total_price = total_price;
+		this.status = status;
+		this.rides = rides;
+		this.users = users;
+	}
+	
+	
+
+	public Booking(LocalDateTime time, int no_of_seats, int total_price, String status, Ride rides, User users) {
+		super();
+		this.time = time;
+		this.no_of_seats = no_of_seats;
+		this.total_price = total_price;
+		this.status = status;
+		this.rides = rides;
+		this.users = users;
+	}
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getTime() {
+		return time;
+	}
+
+	public void setTime(LocalDateTime time) {
+		this.time = time;
+	}
+
+	public int getNo_of_seats() {
+		return no_of_seats;
+	}
+
+	public void setNo_of_seats(int no_of_seats) {
+		this.no_of_seats = no_of_seats;
+	}
+
+	public int getTotal_price() {
+		return total_price;
+	}
+
+	public void setTotal_price(int total_price) {
+		this.total_price = total_price;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Ride getRides() {
+		return rides;
+	}
+
+	public void setRides(Ride rides) {
+		this.rides = rides;
+	}
+
+	public User getUsers() {
+		return users;
+	}
+
+	public void setUsers(User users) {
+		this.users = users;
+	}
+	
+	
+}
---
+++ carpooling_RESTAPI/src/main/java/com/example/carpooling/repositories/RideRepository.java
@@ -0,0 +1,28 @@
+package com.example.carpooling.repositories;
+
+import java.sql.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.carpooling.models.City;
+import com.example.carpooling.models.Ride;
+
+@Repository
+public interface RideRepository extends JpaRepository<Ride, Integer> 
+{
+
+	@Query("select r from Ride r where r.start_location=:start_location and r.end_location=:end_location")
+	public List<Ride> getAllRidesFromOneCityToAnotherCity(City start_location,City end_location);
+	
+	@Query("select r from Ride r where r.start_location=:start_location and r.end_location=:end_location and r.date_of_journey=:date_of_journey")
+	public List<Ride> getAllRidesFromOneCityToAnotherCityByDate(City start_location,City end_location,Date date_of_journey);
+	
+	@Query("select r from Ride r where r.date_of_journey=:date_of_journey")
+	public List<Ride> getAllRidesByDate(Date date_of_journey);
+	
+	@Query("select r from Ride r where r.status=:status")
+	public List<Ride> getAllRidesByStatus(String status);
+}
